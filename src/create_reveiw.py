6f7e1c977e45fa5a309ede2dbce9a0cc951e375f
andrinekirstine <andrine.kirstine@gmail.com>
2022-03-24 14:24:32 +0100

    Update create_reveiw.py

--- src/create_reveiw.py
+++ src/create_reveiw.py
@@ -2,7 +2,7 @@ import argparse
 if __name__ == "__main__":
   parser = argparse.ArgumentParser("Kaffesmaking Creator")
   parser.add_argument("--brukernavn", help="Brukernavnet det blir lagret under")
-  parser.add_argument("--kaffe-navn", help="Navnet på kaffen")
+  parser.add_argument("--kaffenavn", help="Navnet på kaffen")
   parser.add_argument("--poeng", help="Poeng på kaffesmaken")
   parser.add_argument("--brenneri", help="Navnet på brenneriet")
   parser.add_argument("--smaksnotat", help="Notater om kaffen")
@@ -10,7 +10,10 @@ if __name__ == "__main__":
   arguments = parser.parse_args()
 
   # Nå har arguments masse data som du kan bruke slik:
-  print(arguments.username)
-  print(arguments.coffee_name)
+  print(arguments.brukernavn)
+  print(arguments.kaffenavn)
+  print(arguments.poeng)
+  print(arguments.brenneri)
+  print(arguments.smaksnotat)
 
   
