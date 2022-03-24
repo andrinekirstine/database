c7ff3dc399556b71b41ed6e5572f0826c6d54ec4
andrinekirstine <andrine.kirstine@gmail.com>
2022-03-24 14:46:28 +0100

    Update create_reveiw.py

--- src/create_reveiw.py
+++ src/create_reveiw.py
@@ -1,11 +1,11 @@
 import argparse
 if __name__ == "__main__":
   parser = argparse.ArgumentParser("Kaffesmaking Creator")
-  parser.add_argument("--brukernavn", help="Brukernavnet det blir lagret under")
-  parser.add_argument("--kaffenavn", help="Navnet på kaffen")
-  parser.add_argument("--poeng", help="Poeng på kaffesmaken")
-  parser.add_argument("--brenneri", help="Navnet på brenneriet")
-  parser.add_argument("--smaksnotat", help="Notater om kaffen")
+  parser.add_argument("--brukernavn", help="Brukernavnet det blir lagret under", required=True)
+  parser.add_argument("--kaffenavn", help="Navnet på kaffen", required=True)
+  parser.add_argument("--poeng", help="Poeng på kaffesmaken", required=True)
+  parser.add_argument("--brenneri", help="Navnet på brenneriet", required=True)
+  parser.add_argument("--smaksnotat", help="Notater om kaffen", required=True)
 
   arguments = parser.parse_args()
 
